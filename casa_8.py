ff9aeb6998ffbf33f87e10e53601f8135b344774
AngelLeyva7 <agleyva2@miners.utep.edu>
2024-09-23 12:00:58 -0700

    Casa 8

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,11 @@
+import pandas as pd 
 
+file = pd.read_csv('big-mac-full-index.csv')
+
+# 1. Method 4: Using iterrows() method
+for i,r in file.iterrows():
+    print(r['name'], r['iso_a3'], r['currency_code']) 
+    
+    
+# 2. Method 6: Using apply() method    
+print(file.apply(lambda row:  row['dollar_price'], axis = 1))    
